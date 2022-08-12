8928d6e6fbbea560fff3ceade06cf79fee2642bb
Cody Olsen <stipsan@gmail.com>
2022-08-12 13:15:50 +0200

    chore: working on a new preview mode

---
+++ pages/api/preview-token-only.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-process-env */
+
+export default async function preview(req, res) {
+  const secret = process.env.NEXT_PUBLIC_PREVIEW_SECRET
+  // Only require a secret when in production
+  if (!secret && process.env.NODE_ENV === 'production') {
+    throw new TypeError(`Missing NEXT_PUBLIC_PREVIEW_SECRET`)
+  }
+  // Check the secret if it's provided, enables running preview mode locally before the env var is setup
+  if (secret && req.query.secret !== secret) {
+    return res.status(401).json({message: 'Invalid secret'})
+  }
+
+  const token = process.env.SANITY_API_PREVIEW_TOKEN
+  if (!token) {
+    throw new TypeError(`Missing SANITY_API_PREVIEW_TOKEN`)
+  }
+
+  // Enable Preview Mode by setting the cookies
+  res.setPreviewData({
+    title: 'Preview Mode: Token',
+    description:
+      'Uses a viewer token and EventSource polyfill, heavy but highest probability of success',
+    authMode: 'token',
+    token,
+  })
+  res.writeHead(307, {Location: '/'})
+  res.end()
+}
