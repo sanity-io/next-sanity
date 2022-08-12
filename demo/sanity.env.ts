8b00952fd7806416ad1c9aada32b2eaeb669317b
Cody Olsen <stipsan@gmail.com>
2022-08-12 15:22:08 +0200

    fix: ready the first test

--- demo/sanity.env.ts
+++ demo/sanity.env.ts
@@ -1,3 +1,3 @@
+/* eslint-disable no-process-env */
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
-// eslint-disable-next-line no-process-env
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
