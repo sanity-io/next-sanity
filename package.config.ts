e2b7be7fc954b5d791646ee476470dea1546e8e0
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-10-13 11:59:27 +0200

    fix: use `@sanity/pkg-utils` for bundling

---
+++ package.config.ts
@@ -0,0 +1,25 @@
+import url from '@rollup/plugin-url'
+import {defineConfig} from '@sanity/pkg-utils'
+import path from 'path'
+
+export default defineConfig({
+  extract: {
+    rules: {
+      'ae-forgotten-export': 'warn',
+      'ae-incompatible-release-tags': 'warn',
+      'ae-missing-release-tag': 'warn',
+    },
+  },
+
+  minify: false,
+
+  rollup: {
+    plugins: [
+      url({
+        fileName: '[dirname][hash][extname]',
+        sourceDir: path.join(__dirname, 'src'),
+        include: ['**/*.ico', '**/*.svg', '**/*.png', '**/*.jp(e)?g', '**/*.gif', '**/*.webp'],
+      }),
+    ],
+  },
+})
