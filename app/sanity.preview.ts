8c43e2058a03325195cd685e690205338673302c
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-16 16:35:19 +0100

    chore: prepare for release

--- app/sanity.preview.ts
+++ app/sanity.preview.ts
@@ -7,6 +7,9 @@ import {definePreview} from 'src/preview'
 export const usePreview: UsePreview = definePreview({
   projectId,
   dataset,
+  onPublicAccessOnly: () => {
+    throw new Error(`Unable to load preview as you're not logged in`)
+  },
 })
 
 // Re-exporting as this file defines 'use client', which is necessary for it to be usable in a server component
