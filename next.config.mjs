cb019eeb5c264ab3a29a7f6be9e3deb2415e6eee
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-16 20:48:24 +0100

    chore: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // eslint-disable-next-line no-warning-comments
-  // @TODO figure out why the swc minifier breaks preview mode
-  // swcMinify: false,
-
   experimental: {
     appDir: true,
   },
