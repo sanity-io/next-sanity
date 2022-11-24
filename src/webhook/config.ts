1cf0a7295c5e5b4526d0e010936069e9787bca63
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-24 13:30:58 +0100

    fix: set the webhooks runtime to `nodejs` by default

--- src/webhook/config.ts
+++ src/webhook/config.ts
@@ -1,5 +1,18 @@
+import type {PageConfig} from 'next/types'
+
 /**
- * Next.js will by default parse the body, which can lead to invalid signatures
+ * Configurates the API function with the right runtime and body parsing to handle Sanity Webhook events.
  * @public
  */
-export const config = {api: {bodyParser: false}}
+export const config: PageConfig = {
+  api: {
+    /**
+     * Next.js will by default parse the body, which can lead to invalid signatures.
+     */
+    bodyParser: false,
+  },
+  /**
+   * `@sanity/webhook` isn't updated to support the edge runtime yet, and currently requires Node.js APIs such as Buffer.
+   */
+  runtime: 'nodejs',
+}
