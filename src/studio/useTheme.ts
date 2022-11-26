6e35ebcee0659d0dc45e05db945119ed0232ba01
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-26 20:40:58 +0100

    fix: make `config` optional in `useTheme`

--- src/studio/useTheme.ts
+++ src/studio/useTheme.ts
@@ -1,36 +1,12 @@
 import {useMemo} from 'react'
-import {
-  type Config,
-  type SingleWorkspace,
-  type StudioTheme,
-  type WorkspaceOptions,
-  defaultTheme,
-} from 'sanity'
-
-type WithTheme = {
-  theme: StudioTheme
-}
-type SingleWorkspaceWithTheme = Omit<SingleWorkspace, 'theme'> & WithTheme
-type WorkspaceOptionsWithTheme = Omit<WorkspaceOptions, 'theme'> & WithTheme
-
-function isWorkspaces(config: Config): config is WorkspaceOptions[] {
-  return Array.isArray(config)
-}
-
-function isWorkspaceWithTheme(
-  workspace: SingleWorkspace | WorkspaceOptions
-): workspace is SingleWorkspaceWithTheme | WorkspaceOptionsWithTheme {
-  return Boolean(workspace.theme)
-}
+import {type Config, type SingleWorkspace, type StudioTheme, defaultTheme} from 'sanity'
 
 /** @alpha */
-export function useTheme(config: Config): StudioTheme {
-  const workspace = useMemo<SingleWorkspace | WorkspaceOptions>(
-    () => (isWorkspaces(config) ? config[0] : config),
-    [config]
-  )
-  return useMemo<StudioTheme>(
-    () => (isWorkspaceWithTheme(workspace) ? workspace.theme : defaultTheme),
-    [workspace]
-  )
+export function useTheme(
+  config?: Config | Required<Pick<SingleWorkspace, 'theme'>>
+): StudioTheme {
+  const workspace = useMemo<
+    SingleWorkspace | Required<Pick<SingleWorkspace, 'theme'>> | undefined
+  >(() => (Array.isArray(config) ? config[0] : config), [config])
+  return useMemo<StudioTheme>(() => workspace?.theme || defaultTheme, [workspace])
 }
