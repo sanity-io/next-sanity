b8a2210c7ec8fe46acf96c5025a91897d7654a57
Cody Olsen <stipsan@gmail.com>
2022-12-20 16:31:43 +0100

    fix: remove tailwind SVG workaround (#232)
    Co-authored-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- src/studio/NextStudioLayout.tsx
+++ src/studio/NextStudioLayout.tsx
@@ -1,22 +1,16 @@
 /* eslint-disable camelcase */
 import {memo} from 'react'
 import type {StudioProps} from 'sanity'
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
 
 import {useTheme} from './useTheme'
 
 /** @alpha */
 export interface NextStudioLayoutProps extends Pick<StudioProps, 'config' | 'scheme'> {
   children: React.ReactNode
-  /**
-   * Apply fix with SVG icon centering that happens if TailwindCSS is loaded
-   * @defaultValue true
-   */
-  unstable__tailwindSvgFix?: boolean
 }
 
 type LayoutProps = {
-  $unstable__tailwindSvgFix: NextStudioLayoutProps['unstable__tailwindSvgFix']
   $bg: string
   $fontFamily: string
 }
@@ -28,30 +22,18 @@ const Layout = styled.div<LayoutProps>`
   overscroll-behavior: none;
   -webkit-font-smoothing: antialiased;
   overflow: auto;
-
-  ${({$unstable__tailwindSvgFix}: any) =>
-    $unstable__tailwindSvgFix
-      ? css`
-          /* override tailwind reset */
-          *:not([data-ui='Popover__arrow']):not([data-ui='Tooltip__arrow']) > svg {
-            display: inline;
-          }
-        `
-      : ''}
 `
 
 const NextStudioLayoutComponent = ({
   children,
   config,
   scheme = 'light',
-  unstable__tailwindSvgFix = true,
 }: NextStudioLayoutProps) => {
   const theme = useTheme(config)
 
   return (
     <Layout
       data-ui="NextStudioLayout"
-      $unstable__tailwindSvgFix={unstable__tailwindSvgFix}
       $fontFamily={theme.fonts.text.family}
       $bg={theme.color[scheme].default.base.bg}
     >
