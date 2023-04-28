0781d04911e21c11ed1ee4f3aa582b5d4947b6e8
Cody Olsen <stipsan@gmail.com>
2023-04-28 14:07:14 +0200

    chore(e2e): test fast render

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import './globals.css'
 
+import StyledComponentsRegistry from './registry'
+
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
