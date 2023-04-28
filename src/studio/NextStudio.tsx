60ef49e0f0f48461a3bc481bc6c4389aaf717380
Cody Olsen <stipsan@gmail.com>
2023-04-28 13:53:39 +0200

    feat: add `unstable__fastRender` experiment

--- src/studio/NextStudio.tsx
+++ src/studio/NextStudio.tsx
@@ -3,9 +3,10 @@ import {Studio, type StudioProps} from 'sanity'
 
 import {NextStudioClientOnly} from './NextStudioClientOnly'
 import {NextStudioLayout} from './NextStudioLayout'
-import {NextStudioLoading, type NextStudioLoadingProps} from './NextStudioLoading'
+import {NextStudioLoading} from './NextStudioLoading'
+import {NextStudioNoScript} from './NextStudioNoScript'
 
-export type {NextStudioLoadingProps}
+export type {NextStudioLoadingProps} from './NextStudioLoading'
 
 /** @beta */
 export interface NextStudioProps extends StudioProps {
@@ -15,7 +16,13 @@ export interface NextStudioProps extends StudioProps {
    * @defaultValue true
    * @alpha
    */
-  unstable__noScript?: NextStudioLoadingProps['unstable__noScript']
+  unstable__noScript?: boolean
+  /**
+   * Render in a faster mode that requires `styled-components` SSR to be setup.
+   * @defaultValue false
+   * @alpha
+   */
+  unstable__fastRender?: boolean
 }
 /**
  * Intended to render at the root of a page, letting the Studio own that page and render much like it would if you used `npx sanity start` to render
@@ -24,24 +31,45 @@ export interface NextStudioProps extends StudioProps {
 const NextStudioComponent = ({
   children,
   config,
-  unstable__noScript,
+  unstable__noScript = true,
+  unstable__fastRender,
   scheme,
   ...props
-}: NextStudioProps) => (
-  <NextStudioClientOnly
-    fallback={
-      <NextStudioLoading
-        unstable__noScript={unstable__noScript}
-        config={config}
-        scheme={scheme}
-      />
-    }
-  >
-    <NextStudioLayout config={config} scheme={scheme}>
-      {children || <Studio config={config} scheme={scheme} unstable_globalStyles {...props} />}
-    </NextStudioLayout>
-  </NextStudioClientOnly>
-)
+}: NextStudioProps) => {
+  if (unstable__fastRender) {
+    return (
+      <>
+        {unstable__noScript && <NextStudioNoScript />}
+        <NextStudioLayout config={config} scheme={scheme}>
+          {children || (
+            <Studio config={config} scheme={scheme} unstable_globalStyles {...props} />
+          )}
+        </NextStudioLayout>
+      </>
+    )
+  }
+
+  return (
+    <>
+      {unstable__noScript && <NextStudioNoScript />}
+      <NextStudioClientOnly
+        fallback={
+          <NextStudioLoading
+            unstable__noScript={unstable__noScript}
+            config={config}
+            scheme={scheme}
+          />
+        }
+      >
+        <NextStudioLayout config={config} scheme={scheme}>
+          {children || (
+            <Studio config={config} scheme={scheme} unstable_globalStyles {...props} />
+          )}
+        </NextStudioLayout>
+      </NextStudioClientOnly>
+    </>
+  )
+}
 
 /**
  * Override how the Studio renders by passing children.
