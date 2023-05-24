bb448ea55c361936ded8e0d70a0a5a52b8333693
Cody Olsen <cody@sanity.io>
2023-05-24 17:53:43 +0200

    chore: test studioUrl (#444)

--- app/sanity.client.ts
+++ app/sanity.client.ts
@@ -3,4 +3,4 @@ import {createClient as _createClient, type SanityClient} from 'src'
 import {apiVersion, dataset, projectId, useCdn} from './config'
 
 export const createClient = (): SanityClient =>
-  _createClient({projectId, dataset, apiVersion, useCdn})
+  _createClient({projectId, dataset, apiVersion, useCdn, studioUrl: '/studio', logger: console})
