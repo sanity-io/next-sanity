22a3a6611720faf070a4033ba30b68103f716673
Cody Olsen <stipsan@gmail.com>
2023-06-09 05:57:37 +0200

    chore: use `includeTypes` to limit the list

--- app/PreviewProvider.tsx
+++ app/PreviewProvider.tsx
@@ -15,7 +15,7 @@ export default function PreviewProvider({
       projectId={projectId}
       dataset={dataset}
       token={token}
-      documentLimit={Infinity}
+      includeTypes={['author', 'post']}
     >
       {children}
     </GroqStoreProvider>
