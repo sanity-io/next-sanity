ac4a288e24460c83c3d25b51b6de3e23780bd88f
Cody Olsen <stipsan@gmail.com>
2023-08-08 06:44:39 +0200

    fix: `styled-components` getting mangled in `next dev`
    Fixes #515

--- src/studio/NextStudioLayout.tsx
+++ src/studio/NextStudioLayout.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import {memo} from 'react'
 import type {StudioProps} from 'sanity'
-import styled from 'styled-components'
 
 import {useTheme} from './useTheme'
 
@@ -9,20 +8,6 @@ import {useTheme} from './useTheme'
 export interface NextStudioLayoutProps extends Pick<StudioProps, 'config' | 'scheme'> {
   children: React.ReactNode
 }
-
-type LayoutProps = {
-  $bg: string
-  $fontFamily: string
-}
-const Layout = styled.div<LayoutProps>`
-  font-family: ${({$fontFamily}) => $fontFamily};
-  background-color: ${({$bg}: any) => $bg};
-  height: 100vh;
-  max-height: 100dvh;
-  overscroll-behavior: none;
-  -webkit-font-smoothing: antialiased;
-  overflow: auto;
-`
 
 const NextStudioLayoutComponent = ({
   children,
@@ -32,13 +17,20 @@ const NextStudioLayoutComponent = ({
   const theme = useTheme(config)
 
   return (
-    <Layout
+    <div
       data-ui="NextStudioLayout"
-      $fontFamily={theme.fonts.text.family}
-      $bg={theme.color[scheme === 'dark' ? 'dark' : 'light'].default.base.bg}
+      style={{
+        fontFamily: theme.fonts.text.family,
+        backgroundColor: theme.color[scheme === 'dark' ? 'dark' : 'light'].default.base.bg,
+        height: '100vh',
+        maxHeight: '100dvh',
+        overscrollBehavior: 'none',
+        WebkitFontSmoothing: 'antialiased',
+        overflow: 'auto',
+      }}
     >
       {children}
-    </Layout>
+    </div>
   )
 }
 
