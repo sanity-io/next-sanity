65c65a72764d020b9583b3c3082723d1740e664c
Cody Olsen <stipsan@gmail.com>
2024-04-08 15:08:10 +0200

    fix!: remove deprecated `next-sanity/studio/metadata` export

--- packages/next-sanity/src/studio/metadata.ts
+++
@@ -1,9 +0,0 @@
-import type {Metadata} from 'next'
-
-import {metadata as _metadata} from './head'
-
-/**
- * @public
- * @deprecated use `export {metadata} from 'next-sanity/studio'` instead
- */
-export const metadata = _metadata satisfies Metadata
