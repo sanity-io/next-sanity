db2f301be9043b5c76b7100d0ff541583acfa2cb
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-05-22 09:00:46 +0200

    fix: add v19 to `react` peer dep range (#1519)

--- apps/mvp/next.config.mjs
+++ apps/mvp/next.config.mjs
@@ -9,6 +9,18 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_TEST_BASE_PATH,
   experimental: {
     taint: true,
+    reactCompiler: true,
+    turbo: {
+      resolveAlias: {
+        '@sanity/vision': '@sanity/vision/lib/index.mjs',
+        'sanity/_singletons': 'sanity/lib/_singletons.mjs',
+        'sanity/desk': 'sanity/lib/desk.mjs',
+        'sanity/presentation': 'sanity/lib/presentation.mjs',
+        'sanity/router': 'sanity/lib/router.mjs',
+        'sanity/structure': 'sanity/lib/structure.mjs',
+        sanity: 'sanity/lib/index.mjs',
+      },
+    },
   },
   logging: {
     fetches: {
