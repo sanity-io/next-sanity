7f21941897d757ab3190aeda163dd4b30e733d80
Simeon Griggs <simeon@hey.com>
2024-08-20 09:24:32 +0100

    fix: update groq dep and export defineQuery

--- packages/next-sanity/src/index.ts
+++ packages/next-sanity/src/index.ts
@@ -1,4 +1,4 @@
 export * from './client'
 export * from './visual-editing'
 export * from '@portabletext/react'
-export {default as groq} from 'groq'
+export {defineQuery, default as groq} from 'groq'
