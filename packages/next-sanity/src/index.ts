ea4131c861ee830da3c2af6ae1f7093a5431b1ff
Cody Olsen <stipsan@gmail.com>
2024-09-19 14:40:36 +0200

    chore: fix linter

--- packages/next-sanity/src/index.ts
+++ packages/next-sanity/src/index.ts
@@ -1,5 +1,5 @@
 export * from './client'
 export * from './visual-editing'
 export * from '@portabletext/react'
-export {defineQuery, default as groq} from 'groq'
 export * from '@sanity/next-loader'
+export {defineQuery, default as groq} from 'groq'
