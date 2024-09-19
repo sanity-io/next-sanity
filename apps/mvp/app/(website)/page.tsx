bfd348f56a73bfdb965d733fad6c9cefb639b242
Cody Olsen <stipsan@gmail.com>
2024-09-19 19:36:18 +0200

    chore: bye bye preview-kit

--- apps/mvp/app/(website)/page.tsx
+++ apps/mvp/app/(website)/page.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
+import {draftMode} from 'next/headers'
 import Link from 'next/link'
 import {unstable__adapter, unstable__environment} from 'next-sanity'
 import {Suspense} from 'react'
 
-import ConditionalPreviewProvider from './ConditionalPreviewProvider'
-import Posts from './Posts'
+import PostsLayout, {query} from '@/app/(website)/PostsLayout'
 
-export default function IndexPage() {
+import {sanityFetch} from './live'
+
+export default async function IndexPage() {
   return (
     <>
       <div
@@ -20,14 +22,12 @@ export default function IndexPage() {
         <div className="relative mx-auto max-w-7xl">
           <div className="text-center">
             <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Preview Kit
+              Posts {draftMode().isEnabled && '(Draft Mode)'}
             </h2>
           </div>
-          <ConditionalPreviewProvider>
-            <Suspense>
-              <Posts />
-            </Suspense>
-          </ConditionalPreviewProvider>
+          <Suspense>
+            <Posts />
+          </Suspense>
         </div>
       </div>
       <div className="flex text-center">
@@ -37,13 +37,13 @@ export default function IndexPage() {
         >
           Open Studio
         </Link>
-        <Link
-          href="/visual-editing-only"
-          className="mx-2 my-4 inline-block rounded-full border border-gray-200 px-4 py-1 text-sm font-semibold text-gray-600 hover:border-transparent hover:bg-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
-        >
-          Visual Editing Only
-        </Link>
       </div>
     </>
   )
+}
+
+async function Posts() {
+  const {data} = await sanityFetch({query})
+
+  return <PostsLayout data={data} draftMode={draftMode().isEnabled} />
 }
