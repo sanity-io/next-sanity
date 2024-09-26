5620ac6a1487f39a90d5f2d8a613125967a361f1
Cody Olsen <stipsan@gmail.com>
2024-09-26 11:42:01 +0200

    chore: fix build error

--- apps/mvp/app/(sanity)/studio/hash/page.tsx
+++ apps/mvp/app/(sanity)/studio/hash/page.tsx
@@ -2,6 +2,8 @@ import {NextStudio} from 'next-sanity/studio'
 
 import config from '@/sanity.config'
 
+export const dynamic = 'force-static'
+
 export default function StudioPage() {
   return (
     <NextStudio
--- apps/static/app/studio/Studio.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import {NextStudio} from 'next-sanity/studio'
-
-import config from '@/sanity.config'
-
-export default function Studio() {
-  return <NextStudio config={config} history="hash" />
-}
