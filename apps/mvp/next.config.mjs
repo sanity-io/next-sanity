7e890b3d2aaa74f08c4cbcef4454f28515437e69
Cody Olsen <stipsan@gmail.com>
2024-10-14 15:44:30 +0200

    chore: bump deps

--- apps/mvp/next.config.mjs
+++ apps/mvp/next.config.mjs
@@ -31,6 +31,10 @@ const nextConfig = {
       fullUrl: false,
     },
   },
+  env: {
+    // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
+    SC_DISABLE_SPEEDY: 'false',
+  },
   productionBrowserSourceMaps: true,
 
   webpack(config) {
