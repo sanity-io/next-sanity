820715fdccd57385d4a5c7517015d4c893bab605
Cody Olsen <stipsan@gmail.com>
2024-11-07 16:36:03 +0100

    chore: stop prefetching

--- apps/mvp/app/(website)/page.tsx
+++ apps/mvp/app/(website)/page.tsx
@@ -31,6 +31,7 @@ export default async function IndexPage() {
       </div>
       <div className="flex text-center">
         <Link
+          prefetch={false}
           href="/studio"
           className="mx-2 my-4 inline-block rounded-full border border-gray-200 px-4 py-1 text-sm font-semibold text-gray-600 hover:border-transparent hover:bg-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
         >
