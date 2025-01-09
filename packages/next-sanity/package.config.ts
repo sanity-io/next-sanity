5f60d31d1cc113909405fbddfd0fd90f466a9a21
Cody Olsen <stipsan@gmail.com>
2025-01-09 02:24:46 +0100

    chore: fix build error

--- packages/next-sanity/package.config.ts
+++ packages/next-sanity/package.config.ts
@@ -44,7 +44,6 @@ export default defineConfig({
   },
   extract: {
     rules: {
-      'ae-forgotten-export': 'error',
       'ae-incompatible-release-tags': 'warn',
       'ae-internal-missing-underscore': 'off',
       'ae-missing-release-tag': 'warn',
