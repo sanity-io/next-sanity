e28f759f28b2b50013ec2dd823d8581c9c685026
Cody Olsen <stipsan@gmail.com>
2025-01-22 18:38:35 +0100

    chore: bump peer deps

--- packages/next-sanity/vite.config.ts
+++ packages/next-sanity/vite.config.ts
@@ -1,14 +1,10 @@
 import tsconfigPaths from 'vite-tsconfig-paths'
 import {configDefaults, defineConfig} from 'vitest/config'
-import GithubActionsReporter from 'vitest-github-actions-reporter'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     // don't use vitest to run Bun and Deno tests
     exclude: [...configDefaults.exclude, 'test.cjs', 'test.mjs'],
-    // Enable rich PR failed test annotation on the CI
-    // eslint-disable-next-line no-process-env
-    reporters: process.env['GITHUB_ACTIONS'] ? ['default', new GithubActionsReporter()] : 'default',
   },
 })
