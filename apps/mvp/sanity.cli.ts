626a1b33a2dfa67d2b02baa50bb42c40c60b3de9
Cody Olsen <stipsan@gmail.com>
2025-05-07 13:58:13 +0200

    chore: fix deploy mapping

--- apps/mvp/sanity.cli.ts
+++ apps/mvp/sanity.cli.ts
@@ -8,4 +8,7 @@ loadEnvConfig(__dirname, dev, {info: () => null, error: console.error})
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({api: {projectId, dataset}})
+export default defineCliConfig({
+  api: {projectId, dataset},
+  studioHost: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/studio`,
+})
