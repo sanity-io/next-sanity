ea7701b7d68c7964edec0e51613a4cfcbb06cd26
Cody Olsen <stipsan@gmail.com>
2025-05-19 15:31:08 +0200

    chore: test async function as a prop

--- apps/mvp/app/(website)/layout.tsx
+++ apps/mvp/app/(website)/layout.tsx
@@ -4,6 +4,7 @@ import {draftMode} from 'next/headers'
 import {VisualEditing} from 'next-sanity'
 
 import {SanityLive} from './live'
+import {revalidateTag} from 'next/cache'
 
 export default async function RootLayout({children}: {children: React.ReactNode}) {
   return (
@@ -12,7 +13,16 @@ export default async function RootLayout({children}: {children: React.ReactNode}
       <body>
         {children}
         {(await draftMode()).isEnabled && <VisualEditing />}
-        <SanityLive />
+        <SanityLive
+          // Only revalidate when changes to tags currently being rendered
+          revalidateSyncTags={async (tags) => {
+            'use server'
+
+            for (const tag of tags) {
+              await revalidateTag(`sanity:${tag}`)
+            }
+          }}
+        />
       </body>
     </html>
   )
