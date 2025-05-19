375902e22f985037a2ecbcc6e79f39083eda1653
Cody Olsen <stipsan@gmail.com>
2025-05-19 15:01:39 +0200

    chore: test system status

---
+++ apps/mvp/app/(website)/test-cache/SystemStatus.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import useSWR from 'swr'
+
+type StatusResponse = {
+  page?: {
+    id?: string
+    name?: string
+    url?: string
+    time_zone?: string
+    updated_at?: string
+  }
+  status?: {
+    description?: string
+    indicator?: string
+  }
+}
+
+const statusStyleMap: Record<string, string> = {
+  pending: '!text-gray-500-dynamic',
+  none: '!text-green-700-dynamic',
+  critical: '!text-red-700-dynamic',
+  major: '!text-yellow-700-dynamic',
+  minor: '!text-orange-700-dynamic',
+}
+
+export function SystemStatus() {
+  const url = 'https://qzkkn6cg4njg.statuspage.io/api/v2/status.json'
+  const {data} = useJSON<StatusResponse>(url)
+  const isPending = !!data?.status?.indicator
+
+  return (
+    <button color="gray" href={data?.page?.url}>
+      {isPending ? data?.status?.description : 'Loading system status...'}
+    </button>
+  )
+}
+
+function fetchJSON(url: string) {
+  return fetch(url).then((res) => res.json())
+}
+
+export function useJSON<T>(url: Parameters<typeof useSWR>[0]) {
+  return useSWR<T>(url, fetchJSON)
+}
--- apps/mvp/app/(website)/test-cache/page.tsx
+++ apps/mvp/app/(website)/test-cache/page.tsx
@@ -1,14 +1,19 @@
 import {revalidateTag} from 'next/cache'
+import {SystemStatus} from './SystemStatus'
 
 export default function TestCachePage() {
   return (
-    <button
-      onClick={async () => {
-        'use server'
-        await revalidateTag('sanity:s1:57Y4Uw')
-      }}
-    >
-      Test Cache
-    </button>
+    <>
+      <button
+        onClick={async () => {
+          'use server'
+          await revalidateTag('sanity:s1:57Y4Uw')
+        }}
+      >
+        Test Cache
+      </button>
+      <br />
+      <SystemStatus />
+    </>
   )
 }
