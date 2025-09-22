be7adea98bd24f3e8404914f0b722681ebafc1f4
Cody Olsen <stipsan@gmail.com>
2025-09-22 19:43:13 +0200

    refactor: use tsdown

---
+++ packages/next-sanity/tsdown.config.ts
@@ -0,0 +1,34 @@
+import {defineConfig} from 'tsdown'
+
+/**
+ * Testing out tsdown, if it works well we'll move it to `@sanity/pkg-utils`
+ */
+
+export default defineConfig({
+  tsconfig: 'tsconfig.build.json',
+  entry: [
+    './src/draft-mode/index.ts',
+    './src/hooks/index.ts',
+    './src/image/index.ts',
+    './src/index.ts',
+    './src/live.ts',
+    './src/studio/client-component/index.ts',
+    './src/studio/index.ts',
+    './src/visual-editing/client-component/index.ts',
+    './src/visual-editing/index.ts',
+    './src/visual-editing/server-actions/index.ts',
+    './src/webhook/index.ts',
+  ],
+  external: [
+    'next-sanity/studio/client-component',
+    'next-sanity/visual-editing/client-component',
+    'next-sanity/visual-editing/server-actions',
+  ],
+  sourcemap: true,
+  hash: false,
+  exports: true,
+  platform: 'neutral',
+  minify: 'dce-only',
+  publint: true,
+  attw: {profile: 'esmOnly'},
+})
