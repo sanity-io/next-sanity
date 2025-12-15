8810a10ced5113cdacbeb70fdeb52ca5d194b3ca
Cody Olsen <stipsan@gmail.com>
2025-12-15 19:44:18 +0100

    remove `vite-tsconfig-paths` plugin

--- packages/next-sanity/vite.config.ts
+++ packages/next-sanity/vite.config.ts
@@ -1,8 +1,6 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
 import {configDefaults, defineConfig} from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     // don't use vitest to run Bun and Deno tests
     exclude: [...configDefaults.exclude, 'test.cjs', 'test.mjs'],
