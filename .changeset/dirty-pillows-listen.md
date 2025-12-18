a0e4920bd524641a2b8130b83f9e5d33ba03abac
Cody Olsen <stipsan@gmail.com>
2025-12-19 00:05:18 +0100

    Reduce bundlesize of `isCorsOriginError`

---
+++ .changeset/dirty-pillows-listen.md
@@ -0,0 +1,5 @@
+---
+"next-sanity": patch
+---
+
+Reduce bundlesize of `isCorsOriginError`
--- packages/next-sanity/src/isCorsOriginError.ts
+++ packages/next-sanity/src/isCorsOriginError.ts
@@ -1,8 +1,8 @@
-import {CorsOriginError} from '@sanity/client'
+import type {CorsOriginError} from '@sanity/client'
 
 /** @public */
 export function isCorsOriginError(error: unknown): error is CorsOriginError {
-  return error instanceof CorsOriginError
+  return error instanceof Error && error.name === 'CorsOriginError'
 }
 
 export type {CorsOriginError}
