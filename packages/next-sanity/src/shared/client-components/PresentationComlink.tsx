157168955bc2c25a71ae85426dd7404219605326
Cody Olsen <stipsan@gmail.com>
2025-12-18 23:50:14 +0100

    SanityLive + `cacheComponents: true`

--- packages/next-sanity/src/shared/client-components/PresentationComlink.tsx
+++ packages/next-sanity/src/shared/client-components/PresentationComlink.tsx
@@ -1,5 +1,11 @@
 import type {ClientPerspective} from '@sanity/client'
 
+import {
+  setComlink,
+  setComlinkClientConfig,
+  setPerspective,
+} from '#client-components/context'
+import {sanitizePerspective} from '#live/sanitizePerspective'
 import {createNode, createNodeMachine} from '@sanity/comlink'
 import {
   createCompatibilityActors,
@@ -7,49 +13,29 @@ import {
   type LoaderNodeMsg,
 } from '@sanity/presentation-comlink'
 import {setPerspectiveCookie} from 'next-sanity/live/server-actions'
-import {useRouter} from 'next/navigation'
 import {startTransition, useEffect, useEffectEvent} from 'react'
-
-import {
-  setComlink,
-  setComlinkClientConfig,
-  setPerspective,
-  perspective,
-} from '../../live/hooks/context'
-import {sanitizePerspective} from '../../live/utils'
 
 export default function PresentationComlink(props: {
   projectId: string
   dataset: string
-  draftModeEnabled: boolean
+  onPerspective?: (perspective: ClientPerspective) => Promise<void>
 }): React.JSX.Element | null {
-  const {projectId, dataset, draftModeEnabled} = props
-  const router = useRouter()
+  const {projectId, dataset, onPerspective = setPerspectiveCookie} = props
 
   useEffect(() => {
     setComlinkClientConfig(projectId, dataset)
   }, [dataset, projectId])
 
   const handlePerspectiveChange = useEffectEvent(
-    (_perspective: ClientPerspective, signal: AbortSignal) => {
-      const nextPerspective = sanitizePerspective(_perspective, 'drafts')
-      if (draftModeEnabled && perspective.toString() !== nextPerspective.toString()) {
-        setPerspective(nextPerspective)
-        startTransition(() =>
-          setPerspectiveCookie(nextPerspective)
-            .then(() => {
-              if (signal.aborted) return
-              router.refresh()
-            })
-            .catch((reason) =>
-              console.error('Failed to set the preview perspective cookie', reason),
-            ),
-        )
-      }
+    (perspective: ClientPerspective) => {
+        // @TODO remove `setPerspective` util and state
+        setPerspective(sanitizePerspective(perspective, 'drafts'))
+        startTransition(() =>  onPerspective(perspective))
     },
   )
 
   useEffect(() => {
+    const controller = new AbortController()
     const comlink = createNode<LoaderNodeMsg, LoaderControllerMsg>(
       {name: 'loaders', connectTo: 'presentation'},
       createNodeMachine<LoaderNodeMsg, LoaderControllerMsg>().provide({
@@ -57,17 +43,17 @@ export default function PresentationComlink(props: {
       }),
     )
 
-    let controller: AbortController | undefined
     comlink.on('loader/perspective', (data) => {
-      controller?.abort()
-      controller = new AbortController()
-      handlePerspectiveChange(data.perspective, controller.signal)
+      if(controller.signal.aborted) return
+
+      handlePerspectiveChange(data.perspective)
     })
 
     const stop = comlink.start()
     setComlink(comlink)
     return () => {
       stop()
+      controller.abort()
     }
   }, [])
 
