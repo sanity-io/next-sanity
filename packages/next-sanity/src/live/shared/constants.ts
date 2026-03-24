6db515e9ee0a9477017797a232c43f0555f3fa7b
Cody Olsen <stipsan@gmail.com>
2026-03-24 22:50:53 +0100

    Changed default prefixes from `sp:` to `sanity:` and `sd:` to `sanity:drafts:`

--- packages/next-sanity/src/live/shared/constants.ts
+++ packages/next-sanity/src/live/shared/constants.ts
@@ -1,11 +1,9 @@
-import type { CacheTagPrefixes } from "./types"
+import type {CacheTagPrefixes} from './types'
 
 /**
  * Sanity Live handles on-demand revalidation, so the default 15min time based revalidation is too short
  */
 export const revalidate = 31_536_000 // 365 days
-
-
 
 export const cacheTagPrefixes = {
   published: 'sanity:',
