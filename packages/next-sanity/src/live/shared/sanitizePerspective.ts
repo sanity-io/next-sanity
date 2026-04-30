3e0009047db4c61de127f30e50d5fc68510f691e
Cody Olsen <stipsan@gmail.com>
2026-04-30 16:16:48 +0200

    refactor: move exports to align with `cache-components` branch

---
+++ packages/next-sanity/src/live/shared/sanitizePerspective.ts
@@ -0,0 +1,18 @@
+import {validateApiPerspective, type ClientPerspective} from '@sanity/client'
+
+export function sanitizePerspective(
+  _perspective: unknown,
+  fallback: 'drafts' | 'published',
+): Exclude<ClientPerspective, 'raw'> {
+  const perspective =
+    typeof _perspective === 'string' && _perspective.includes(',')
+      ? _perspective.split(',')
+      : _perspective
+  try {
+    validateApiPerspective(perspective)
+    return perspective === 'raw' ? fallback : perspective
+  } catch (err) {
+    console.warn(`Invalid perspective:`, _perspective, perspective, err)
+    return fallback
+  }
+}
--- packages/next-sanity/tsdown.config.ts
+++ packages/next-sanity/tsdown.config.ts
@@ -11,10 +11,10 @@ export default defineConfig({
     './src/hooks/index.ts',
     './src/image/index.ts',
     './src/index.ts',
-    './src/live.ts',
-    './src/live.server-only.ts',
-    './src/live/client-components/live/index.ts',
-    './src/live/server-actions/index.ts',
+    './src/live/client-components/index.ts',
+    './src/live/conditions/default/index.ts',
+    './src/live/conditions/react-server/index.ts',
+    './src/live/server-actions/index.default.ts',
     './src/studio/client-component/index.ts',
     './src/studio/index.ts',
     './src/visual-editing/client-component/index.ts',
@@ -54,10 +54,16 @@ export default defineConfig({
       delete pkg['./package.json']
 
       pkg['./live'] = {
-        'react-server': pkg['./live'],
-        'default': pkg['./live.server-only'],
+        'react-server': pkg['./live/conditions/react-server'],
+        'default': pkg['./live/conditions/default'],
       }
-      delete pkg['./live.server-only']
+      delete pkg['./live/conditions/default']
+      delete pkg['./live/conditions/react-server']
+
+      pkg['./live/server-actions'] = {
+        default: pkg['./live/server-actions/index.default'],
+      }
+      delete pkg['./live/server-actions/index.default']
 
       const sortedPkg = {} as typeof pkg
       Object.keys(pkg)
