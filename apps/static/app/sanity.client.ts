f1e983951cbc78c77c2fe26eb7eecaba7e048692
pedrobonamin <pedrobonamin@gmail.com>
2026-07-22 12:53:06 +0200

    fix(apps): update client to variants client

--- apps/static/app/sanity.client.ts
+++ apps/static/app/sanity.client.ts
@@ -1,9 +1,10 @@
 import {createClient} from 'next-sanity'
+import {VARIANTS_STUDIO_CLIENT_OPTIONS} from 'sanity'
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: '2025-03-04',
+  apiVersion: VARIANTS_STUDIO_CLIENT_OPTIONS.apiVersion,
   useCdn: false,
   perspective: 'published',
   resultSourceMap: 'withKeyArraySelector',
